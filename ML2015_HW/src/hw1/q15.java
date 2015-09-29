b2ee8edc27fe1f445919a5c0d83370ffa735b031
CSu <likisee@gmail.com>
2015-09-29 08:09:01 +0800

    init

---
+++ ML2015_HW/src/hw1/q15.java
@@ -0,0 +1,10 @@
+package hw1;
+
+public class q15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ML2015_HW/src/hw1/q18.java
@@ -0,0 +1,10 @@
+package hw1;
+
+public class q18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
