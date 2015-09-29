2601476ada4875ef61c5c1dbe73f0a49628f4b9a
CSu <likisee@gmail.com>
2015-09-29 08:58:53 +0800

    add write

--- ML2015_HW/src/Util/FileUtil.java
+++ ML2015_HW/src/Util/FileUtil.java
@@ -21,10 +21,24 @@ public class FileUtil {
 			return new ArrayList<String>();
 		}
 	}
+	
+	public static void writeFileContent(String filePath, String content, String encoding, boolean append) {
+		writeFileContent(new File(filePath), content, encoding, append);
+	}
+	
+	public static void writeFileContent(File file, String content, String encoding, boolean append) {
+		encoding = encoding==null?"UTF-8":encoding;
+		try {
+			FileUtils.write(file, content, encoding, append);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		FileUtil.writeFileContent("D:\\HAPPY.TXT", "HelloWorld", null, false);
 	}
 
 }
