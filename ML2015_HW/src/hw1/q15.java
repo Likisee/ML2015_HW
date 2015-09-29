987be5055e0348d7045f576d3778a5d359884470
CSu <likisee@gmail.com>
2015-09-30 07:02:46 +0800

    finish Q15~Q17

--- ML2015_HW/src/hw1/q15.java
+++ ML2015_HW/src/hw1/q15.java
@@ -1,5 +1,6 @@
 package hw1;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -9,106 +10,151 @@ import Util.ContainerUtil;
 import Util.FileUtil;
 
 public class q15 {
-
-	private static int getSign(double threshold, double value) {
-		if(value - threshold> 0) {
+	
+	private static Random rand = new Random();
+	
+	private static int getSign(double threshold, double weightSum) {
+		if (weightSum - threshold > 0) {
 			return 1;
 		} else {
 			return -1;
 		}
 	}
-	
-	private static double getWeightSum(double weight, double ... values) {
+
+	private static double getWeightSum(List<Double> weights, List<Double> values) {
 		double weightSum = 0.0;
-		for (double value : values) {
-			weightSum += weight * value;
+		int upper = Math.min(weights.size(), values.size());
+		for (int i = 0; i < upper; i++) {
+			weightSum += weights.get(i) * values.get(i);
 		}
 		return weightSum;
 	}
-	
-	private static double getWeight(double weight, String trainingData) {
+
+	private static List<Double> getWeight(List<Double> weights, String trainingData, double factor) {
+		List<Double> data = new ArrayList<Double>();
+		data.add(1.0);
+
 		trainingData = trainingData.replace(" ", "\t");
-		List<Double> data = ContainerUtil.getRealList(trainingData, "\t");
-		
-		double weightSum = getWeightSum(weight, data.get(0).doubleValue(), data.get(1).doubleValue(), data.get(2).doubleValue(), data.get(3).doubleValue());
+		data.addAll(ContainerUtil.getRealList(trainingData, "\t"));
+
+		double weightSum = getWeightSum(weights, data);
 		int sign = getSign(0, weightSum);
-		
-		if(sign == data.get(4).intValue()) {
-			return weight;
+
+		if (sign == data.get(5).intValue()) {
+			return weights;
 		} else {
-			return weight + getWeightSum(data.get(4).intValue(), data.get(0).doubleValue(), data.get(1).doubleValue(), data.get(2).doubleValue(), data.get(3).doubleValue());
+			double w0 = weights.get(0) + factor * data.get(0) * data.get(5);
+			double w1 = weights.get(1) + factor * data.get(1) * data.get(5);
+			double w2 = weights.get(2) + factor * data.get(2) * data.get(5);
+			double w3 = weights.get(3) + factor * data.get(3) * data.get(5);
+			double w4 = weights.get(4) + factor * data.get(4) * data.get(5);
+			weights = new ArrayList<Double>();
+			weights.add(w0);
+			weights.add(w1);
+			weights.add(w2);
+			weights.add(w3);
+			weights.add(w4);
+			return weights;
 		}
 	}
 	
-	private static double getWeightQ17(double weight, String trainingData, double factor) {
-		trainingData = trainingData.replace(" ", "\t");
-		List<Double> data = ContainerUtil.getRealList(trainingData, "\t");
-		
-		double weightSum = getWeightSum(weight, data.get(0).doubleValue(), data.get(1).doubleValue(), data.get(2).doubleValue(), data.get(3).doubleValue());
-		int sign = getSign(0, weightSum);
-		
-		if(sign == data.get(4).intValue()) {
-			return weight;
-		} else {
-			return weight + factor * getWeightSum(data.get(4).intValue(), data.get(0).doubleValue(), data.get(1).doubleValue(), data.get(2).doubleValue(), data.get(3).doubleValue());
-		}
+	private static List<Double> getNewWeights() {
+		List<Double> weights = new ArrayList<Double>();
+		weights.add(0.0);
+		weights.add(0.0);
+		weights.add(0.0);
+		weights.add(0.0);
+		weights.add(0.0);
+		return weights;
 	}
 	
+	private static List<String> getRandomLines(List<String> lines) {
+		List<String> result = new ArrayList<String>();
+		int seedIndex = rand.nextInt(lines.size());
+		for (int i = seedIndex; i < lines.size(); i++) {
+			result.add(lines.get(i));
+		}
+		for (int i = 0; i < seedIndex; i++) {
+			result.add(lines.get(i));
+		}
+		//Collections.shuffle(result); // seems NOT necessary
+		return result;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
 		// read the training data
 		List<String> lines = FileUtil.readFileContent(CommonUtil.getHwFile("hw1", "hw1_15_train.dat"), null);
-		
+
 		// PLA: Q15
+//		List<Double> weights = getNewWeights();
+//		int itr = 1;
+//		int finalUpdateCnt = 0;
 //		int updateCnt = 0;
-//		double w = 0.0;
 //		for (int i = 0; i < lines.size(); i++) {
-//			double newW = getWeight(w, (String)lines.get(i));
-//			if(w != newW) {
-//				System.out.println("line: " + (i+1) + ", " + "w: " + w + ", " + "newW: " + newW);
-//				updateCnt ++;
-//				w = newW;
+//			List<Double> newWeights = getWeight(weights, (String) lines.get(i), 1.0);
+//			if ((weights.get(0).doubleValue() != newWeights.get(0).doubleValue()) 
+//					|| (weights.get(1).doubleValue() != newWeights.get(1).doubleValue()) 
+//					|| (weights.get(2).doubleValue() != newWeights.get(2).doubleValue()) 
+//					|| (weights.get(3).doubleValue() != newWeights.get(3).doubleValue()) 
+//					|| (weights.get(4).doubleValue() != newWeights.get(4).doubleValue())) {
+//				System.out.println("itr: " + itr + ", " + "line: " + (i + 1) + ", " + "weights: " + weights + ", " + "newWeights: " + newWeights);
+//				updateCnt++;
+//				weights = newWeights;
 //			} else {
-//				System.out.println("line: " + (i+1) + ", PASS");
+//				System.out.println("itr: " + itr + ", " + "line: " + (i + 1) + ", PASS");
+//			}
+//
+//			// halts?
+//			System.out.println("itr: " + itr + ", " + "updateCnt: " + updateCnt);
+//			if (i == lines.size() - 1) {
+//				if (finalUpdateCnt != updateCnt) {
+//					itr++;
+//					finalUpdateCnt = updateCnt;
+//					i = -1; // new for-loop
+//				} else {
+//					break; // no new update -> halts
+//				}
 //			}
 //		}
-//		System.out.println("updateCnt: " + updateCnt);
-		
+//		System.out.println("finalUpdateCnt: " + finalUpdateCnt);
+
 		// PLA: Q16
-		Random rand = new Random();
 		int sumUpdateCnt = 0;
-		for(int round = 0; round < 2000; round++) {
-			int seedIndex = rand.nextInt(400);
+		for (int round = 0; round < 2000; round++) {
+			List<String> tmpLines = getRandomLines(lines);
+			List<Double> weights = getNewWeights();
+			int finalUpdateCnt = 0;
 			int updateCnt = 0;
-			
-			Collections.shuffle(lines);
-			double w = 0.0;
-			for (int i = seedIndex; i < lines.size(); i++) {
-//				double newW = getWeight(w, (String)lines.get(i));
-				double newW = getWeightQ17(w, (String)lines.get(i), 0.5);
-				if(w != newW) {
-					updateCnt ++;
-					w = newW;
+
+			for (int i = 0; i < tmpLines.size(); i++) {
+				//List<Double> newWeights = getWeight(weights, (String) tmpLines.get(i), 1.0);
+				List<Double> newWeights = getWeight(weights, (String)tmpLines.get(i), 0.5);
+				if ((weights.get(0).doubleValue() != newWeights.get(0).doubleValue()) 
+						|| (weights.get(1).doubleValue() != newWeights.get(1).doubleValue()) 
+						|| (weights.get(2).doubleValue() != newWeights.get(2).doubleValue()) 
+						|| (weights.get(3).doubleValue() != newWeights.get(3).doubleValue()) 
+						|| (weights.get(4).doubleValue() != newWeights.get(4).doubleValue())) {
+					updateCnt++;
+					weights = newWeights;
 				}
-			}			
-			for (int i = 0; i < seedIndex; i++) {
-//				double newW = getWeight(w, (String)lines.get(i));
-				double newW = getWeightQ17(w, (String)lines.get(i), 0.5);
-				if(w != newW) {
-					updateCnt ++;
-					w = newW;
+
+				// halts?
+				if (i == lines.size() - 1) {
+					if (finalUpdateCnt != updateCnt) {
+						finalUpdateCnt = updateCnt;
+						i = -1; // new for-loop
+					} else {
+						break; // no new update -> halts
+					}
 				}
 			}
-			System.out.println("round: " + round + ", " + "updateCnt: " + updateCnt);
-			
+
 			sumUpdateCnt = sumUpdateCnt + updateCnt;
-			System.out.println("sumUpdateCnt: " + sumUpdateCnt);
+			System.out.println("round: " + (round + 1) + ", " + "finalUpdateCnt: " + finalUpdateCnt + ", " + "sumUpdateCnt: " + sumUpdateCnt);
 		}
 		System.out.println();
 		System.out.println("avgUpdateCnt: " + (sumUpdateCnt / 2000.0));
-		
 
 	}
-
 }
