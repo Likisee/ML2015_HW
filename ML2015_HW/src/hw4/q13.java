bce5cbc4ec1ff6d0aeb0c3acb0bb0041c5c7a32c
CSu <likisee@gmail.com>
2015-11-22 21:43:57 +0800

    modify the function interface

--- ML2015_HW/src/hw4/q13.java
+++ ML2015_HW/src/hw4/q13.java
@@ -11,11 +11,6 @@ import Util.FileUtil;
 
 public class q13 {
 
-	private static int N = 0;
-	private static int dim = 0;
-	private static double [][] X = null;
-	private static double [] y = null;
-		
 	public static double getSign(double output) {
 		if(output > 0) {
 			return 1.0;
@@ -24,35 +19,62 @@ public class q13 {
 		}
 	}
 	
-	public static void initData(List<String> linesTrain) {
+	public static double [][] initDataX(List<String> lines) {
 		
 		// get N & dim
-		N = linesTrain.size();
-		dim = ("1 " + linesTrain.get(0).trim()).split(" ").length - 1;
+		int N = lines.size();
+		int dim = ("1 " + lines.get(0).trim()).split(" ").length - 1;
 		
 		// initiate
-		X = new double [N][dim];
-		y = new double [N];
+		double [][] X = new double [N][dim];
+		double [] y = new double [N];
 		
 		// initiate values
 		for(int i = 0; i < N; i++) {
-			String [] xy = ("1 " + linesTrain.get(i)).trim().split(" ");
+			String [] xy = ("1 " + lines.get(i)).trim().split(" ");
 			for(int j = 0; j < dim; j++) {
 				X[i][j] = Double.valueOf(xy[j]);
 			}
 			y[i] = Double.valueOf(xy[dim]);
 		}
+		return X;
 	}
 	
-	public static double[] getWReg(double lambda) {
+	public static double [] initDataY(List<String> lines) {
+		
+		// get N & dim
+		int N = lines.size();
+		int dim = ("1 " + lines.get(0).trim()).split(" ").length - 1;
+		
+		// initiate
+		double [][] X = new double [N][dim];
+		double [] y = new double [N];
+		
+		// initiate values
+		for(int i = 0; i < N; i++) {
+			String [] xy = ("1 " + lines.get(i)).trim().split(" ");
+			for(int j = 0; j < dim; j++) {
+				X[i][j] = Double.valueOf(xy[j]);
+			}
+			y[i] = Double.valueOf(xy[dim]);
+		}
+		return y;
+	}
+	
+	public static double[] getWReg(double [][] X, double [] y, double lambda) {
 		RealMatrix xMatrix 	= MatrixUtils.createRealMatrix(X);
 		RealMatrix xMatrixT = xMatrix.transpose();
 		RealMatrix yMatrix 	= MatrixUtils.createColumnRealMatrix(y);
-		RealMatrix iMatrix 	= MatrixUtils.createRealIdentityMatrix(dim); // RealMatrix iMatrix = MatrixUtils.createRealIdentityMatrix(xMatrix.getColumnDimension());
+		RealMatrix iMatrix 	= MatrixUtils.createRealIdentityMatrix(xMatrix.getColumnDimension());
 		return new LUDecomposition(xMatrixT.multiply(xMatrix).add(iMatrix.scalarMultiply(lambda))).getSolver().getInverse().multiply(xMatrixT).multiply(yMatrix).getColumn(0);
 	}
 	
-	public static double getE(double [] wReg) {
+	public static double getE(double [][] X, double [] y, double [] wReg) {
+		
+		// get N & dim
+		int N = X.length;
+		int dim = X[0].length;
+		
 		double errCnt = 0;
 		double s = 0;
 		for(int i = 0; i < N; i++) {
@@ -70,8 +92,17 @@ public class q13 {
 		List<String> linesTrain = FileUtil.readFileContent(CommonUtil.getHwFile("hw4", "hw4_train.dat"), null);
 		List<String> linesTest = FileUtil.readFileContent(CommonUtil.getHwFile("hw4", "hw4_test.dat"), null);
 		
+		double [][] XTrain = null;
+		double [] yTrain = null;
+		XTrain = initDataX(linesTrain);
+		yTrain = initDataY(linesTrain);
+		
+		double [][] XTest = null;
+		double [] yTest = null;
+		XTest = initDataX(linesTest);
+		yTest = initDataY(linesTest);
+		
 		double [] wReg;
-		double[] lambdaArr = { 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 };
 		double lambda;
 		double bestEinLambda;
 		double bestEin;
@@ -79,32 +110,29 @@ public class q13 {
 		double bestEout;		
 		
 		// Q13
-		System.out.println();
-		System.out.println("Q13");
-		initData(linesTrain);
-		lambda = 11.26;
-		wReg = getWReg(lambda);
-		System.out.println("Lambda: " + lambda);
-		System.out.println("wReg: [" + wReg[0] + ", " + wReg[1] + ", " + wReg[2] + "]");
-		System.out.println("Ein: " + getE(wReg));
-		initData(linesTest);
-		System.out.println("Eout: " + getE(wReg));
+//		System.out.println();
+//		System.out.println("Q13");
+//		lambda = 11.26;
+//		wReg = getWReg(XTrain, yTrain, lambda);
+//		System.out.println("Lambda: " + lambda);
+//		System.out.println("wReg: [" + wReg[0] + ", " + wReg[1] + ", " + wReg[2] + "]");
+//		System.out.println("Ein: " + getE(XTrain, yTrain, wReg));
+//		System.out.println("Eout: " + getE(XTest, yTest, wReg));
 		
-		// Q14 & Q15: best Ein (min Ein)
+		// Q14 & Q15
+//		double[] lambdaArr = { 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 };
 //		System.out.println();
 //		System.out.println("Q14 & Q15");
 //		bestEinLambda = 0;
 //		bestEin = Double.MAX_VALUE;
 //		bestEoutLambda = 0;
 //		bestEout = Double.MAX_VALUE;
-//		initData(linesTrain);
 //		for(int i = 0; i < lambdaArr.length; i++) {
-//			initData(linesTrain);
 //			lambda = Math.pow(10, lambdaArr[i]);
-//			wReg = getWReg(lambda);
+//			wReg = getWReg(XTrain, yTrain, lambda);
 ////			System.out.println("Lambda: " + lambda);
 ////			System.out.println("wReg: [" + wReg[0] + ", " + wReg[1] + ", " + wReg[2] + "]");
-//			double ein = getE(wReg);
+//			double ein = getE(XTrain, yTrain, wReg);
 //			if (ein < bestEin) {
 //				System.out.print("Ein: " + ein + " (A BETTER FOUND!!)");
 //				bestEinLambda = lambda;
@@ -113,8 +141,7 @@ public class q13 {
 //				System.out.print("Ein: " + ein);
 //			}
 //			
-//			initData(linesTest);
-//			double eout = getE(wReg);
+//			double eout = getE(XTest, yTest, wReg);
 //			if (eout < bestEout) {
 //				System.out.println(", Eout: " + eout + " (A BETTER FOUND!!)");
 //				bestEoutLambda = lambda;
@@ -131,80 +158,6 @@ public class q13 {
 
 		
 		
-		
-		
-		
-		
-		
-//		double ita = 0;
-//		double T = 0;
-//		ArrayList<Double> wFinal = null;
-//		
-//		
-//		
-//		// Q18: Implement the fixed learning rate gradient descent algorithm
-//		ita = 0.001;
-//		T = 2000;
-//		wFinal = new ArrayList<Double>();
-//		
-//		initData(linesTrain);
-//		w = new double [dim]; // default 0.0
-//		for(int i = 0; i < T; i++) {
-//			gradient = new double[dim];	// default 0.0
-//			updateGradient();
-//			updateW(ita);
-//		}
-//		for(int j = 0; j < dim; j++) {
-//			wFinal.add(w[j]);
-//		}
-//		System.out.println("w: " + wFinal);
-//		
-//		initData(linesTest);
-//		System.out.println("eOut: " + getEOut());
-//		
-//		
-//		
-//		// Q19: Implement the fixed learning rate gradient descent algorithm
-//		ita = 0.01;
-//		T = 2000;
-//		wFinal = new ArrayList<Double>();
-//		
-//		initData(linesTrain);
-//		w = new double [dim]; // default 0.0
-//		for(int i = 0; i < T; i++) {
-//			gradient = new double[dim];	// default 0.0
-//			updateGradient();
-//			updateW(ita);
-//		}
-//		for(int j = 0; j < dim; j++) {
-//			wFinal.add(w[j]);
-//		}
-//		System.out.println("w: " + wFinal);
-//		
-//		initData(linesTest);
-//		System.out.println("eOut: " + getEOut());
-//		
-//		
-//		
-//		// Q20: Implement the fixed learning rate stochastic gradient descent algorithm
-//		ita = 0.001;
-//		T = 2000;
-//		wFinal = new ArrayList<Double>();
-//		
-//		initData(linesTrain);
-//		w = new double [dim]; // default 0.0
-//		for(int i = 0; i < T; i++) {
-//			gradient = new double[dim];	// default 0.0
-//			updateGradientStochastic(i);
-//			updateW(ita);
-//		}
-//		for(int j = 0; j < dim; j++) {
-//			wFinal.add(w[j]);
-//		}
-//		System.out.println("w: " + wFinal);
-//		
-//		initData(linesTest);
-//		System.out.println("eOut: " + getEOut());
 		
 	}
 
