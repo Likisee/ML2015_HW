5462c61905bbd4e30e59c4001f4e135176c4430b
CSu <likisee@gmail.com>
2015-12-09 02:17:49 +0800

    hw5 q20 batch files

---
+++ ML2015_HW/ntumlone/hw5/q20_0.bat
@@ -0,0 +1,6 @@
+c:
+cd \
+cd C:\libsvm-3.20\windows\
+svm-train.exe -t 2 -d 2 -g 1 -c 0.1 q20_train_1000.txt
+svm-predict.exe q20_rest.txt q20_train_1000.txt.model output.txt > q20_eval_result.txt
+exit(1)
